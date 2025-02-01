9205555a1d71e10996b623130f9bcec1228916d2
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-02-01 20:21:00 +0500

    update

--- modules/fileSystem.js
+++ modules/fileSystem.js
@@ -1,10 +1,33 @@
 const fs = require("fs");
-const path = require("path")
+const path = require("path");
 
-fs.mkdir(path.join(__dirname, "tamplates"), err=>{
-    if (err) throw new  Error();
+// fs.mkdir(path.join(__dirname, "templates"), err => {
+//     if (err) throw new Error()
+//     console.log("file was created succesfuly")
+// })
 
-    console.log("Fayl yaratildi")
+// fs.mkdir(path.join(__dirname, "notes"), err => {
+//     if (err) throw new Error();
+
+//     console.log("notes yaratildi")
+// })
+
+fs.writeFile(path.join(__dirname, "notes", "javohir.txt"), "Suvonov Javohir", err => {
+    if (err) throw new Error;
+
+    console.log("papka yartildi va text yozildi ")
+
+    fs.appendFile(path.join(__dirname, "notes", "javohir.txt"), " Alimardon o'g'li ", err => {
+        if (err) throw new Error()
+
+        console.log("o'zgartirish kiritildi")
+        fs.readFile(path.join(__dirname, "notes", "javohir.txt"), "utf-8", (err, data) => {
+            if (err) throw new Error()
+            console.log(data)
+        })
+
+    })
+
 })
 
 
