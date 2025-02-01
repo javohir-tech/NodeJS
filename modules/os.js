35f4714e45056b51d524c1b9cdb342c1c4533cdc
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-02-01 23:12:40 +0500

    os operation system

---
+++ modules/os.js
@@ -0,0 +1,17 @@
+const os = require("os")
+//platform
+// console.log(os.platform())
+//information
+// console.log(os.cpus())
+
+//archicture
+// console.log(os.arch())
+
+// band joy
+// console.log((os.freemem() / (1024 * 1024 * 1024)).toFixed(2) + ' GB');
+//tottal mem
+// console.log((os.totalmem())/(1024*1024*1024).toFixed(2) + "gb")
+
+//point
+console.log(os.homedir())
+
