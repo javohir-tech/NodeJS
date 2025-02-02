2f25af0439390af970a330c6782dcabe127a5bfa
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-02-02 22:13:47 +0500

    get post

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-const http = require("http");
+const http = require("http")
 
-const server = http.createServer((request, response)=>{
-    console.log(request.url)
+const server = http.createServer((req, res) => {
+    if (req.method === "GET") {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(`
+        <h2>Ismingizni jo'nating</h2>  
+        <form method="post", action="/">
+            <input type="text" name="name" placeholder="Ismingizni Kiriting"/>
+            <button type="submit">Send</button>
+        </form>      
+        `)
+    } else if (req.method === "POST") {
+        const body = []
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" })
+        req.on('data', data => {
+            body.push(Buffer.from(data))
+        })
 
-    response.write("<h1>Salom Dunyo3</h1>")
-    response.write("<h1>Salom Dunyo34</h1>")
-    response.end()
+        req.on('end', () => {
+            const message = body.toString().split("=")[1]
+            res.end(`salom hurmatli mijoz : ${message}`)
+        })
+    }
 })
 
-server.listen(2700, ()=>{
-    console.log("Server has been started port : 2700")
+server.listen(3000, () => {
+    console.log("server has been started port :3000")
 })
