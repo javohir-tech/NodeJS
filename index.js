7b78afe9cf60a188217f6825b85f529993636720
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-02-02 23:40:15 +0500

    update

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const http = require("http")
+const http = require('http');
+const { buffer } = require('stream/consumers');
 
 const server = http.createServer((req, res) => {
     if (req.method === "GET") {
-        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.writeHead(200, { "Content-Type": "text/html" })
         res.end(`
-        <h2>Ismingizni jo'nating</h2>  
+         <h2>Ismingizni jo'nating</h2>  
         <form method="post", action="/">
             <input type="text" name="name" placeholder="Ismingizni Kiriting"/>
             <button type="submit">Send</button>
-        </form>      
+        </form>
         `)
     } else if (req.method === "POST") {
+        res.writeHead(200, { "Content-Type": "text/html, chaarset='utf-8'" })
+
         const body = []
-        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" })
-        req.on('data', data => {
+
+        req.on("data", data => {
             body.push(Buffer.from(data))
+            console.log(body.toString())
         })
 
-        req.on('end', () => {
+        req.on("end", () => {
             const message = body.toString().split("=")[1]
-            res.end(`salom hurmatli mijoz : ${message}`)
+            res.end(`salom foydalanuvchi ${message}`)
         })
+
     }
 })
 
 server.listen(3000, () => {
-    console.log("server has been started port :3000")
+    console.log(`server has been started port : 3000`)
 })
